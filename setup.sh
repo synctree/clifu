b1c21b1cdc37577750810202447842be464d805c
Masahji Stewart <masahji@synctree.com>
2011-03-31 16:19:02 -0700

    running packages before profiles

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 CLIFU_HOME=${CLIFU_HOME:-$HOME/.clifu};
 
-mkdir -p $CLIFU_HOME/profile.d
-for file in `find $CLIFU_HOME/profile.d -name '*.sh'`
+for file in `find $CLIFU_HOME/packages.d -name '*.sh'`
 do
   source $file
 done
 
-for file in `find $CLIFU_HOME/packages.d -name '*.sh'`
+mkdir -p $CLIFU_HOME/profile.d
+for file in `find $CLIFU_HOME/profile.d -name '*.sh'`
 do
   source $file
 done
